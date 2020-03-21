61b7ad13ef46d0ae32fad37cda5519bfebeb9926
socrat12 <sergskurat@gmail.com>
2020-03-21 18:13:35 +0300

    Reader

---
+++ src/net/Level3/ThreeEights.java
@@ -0,0 +1,13 @@
+package net.Level3;
+
+public class ThreeEights {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i ++){
+            System.out.println();
+            for (int x = 0; x <= i; x ++){
+                System.out.print("8");
+
+            }
+        }
+    }
+}
